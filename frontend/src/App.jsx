f167625931d9b8b3028c27f1e3be7ab479a6a603
BouzirJawad <jawadbouzir01@gmail.com>
2025-09-15 00:30:33 +0100

    app file, and updated packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
+import React from 'react'
+import AuthProvider from './provider/AuthProvider'
+import Routes from "./routes/index"
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   return (
-    <div className=''>
-      hello
-    </div>
+    <AuthProvider>
+      <Toaster position='top-center' />
+      <Routes />
+    </AuthProvider>
   )
 }
 
